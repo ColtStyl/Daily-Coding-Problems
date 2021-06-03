0f2074df40c773a92172561b196a2caa220cc0f6
Benjamin MARTIN <benjamin.martin1@edu.ece.fr>
2021-06-03 21:49:45 +0200

    Added problem #800, and also a .gitignore

---
+++ daily-800/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+// To view the problem this solution belongs to and understand the solution better, read the README.
+
+namespace daily_800
+{
+
+    // Simple Node implementation
+    class Node
+    {
+           public int data;
+           public Node next;
+
+           public Node(){}
+           public Node(int d)
+           {
+               data = d;
+               next = null;
+           }
+        
+    }
+
+    // Simple LinkedList implementation
+    class SingleLinkedList
+    {
+        private Node head;
+
+        public void AddNode(int data)
+        {
+            Node newNode = new Node(data);
+            if(head == null) {head = newNode; return;}
+            
+            Node foo = head;
+            while(foo.next != null)
+            {
+                foo = foo.next;
+            }
+
+            foo.next = newNode;
+        }
+
+        public void Rearrange()
+        {
+            // If the linked list has no nodes or only one, we just return
+            if(head.next == null || head == null) return;
+
+            // The counter is used to count the number of values we already have modified
+            int i = 0, counter = 0;
+            List<int> valuesVector = new List<int>();
+            Node foo = head;
+
+            // We add all node values into our vector until we hit the last node
+            while(foo.next != null)
+            {
+                valuesVector.Add(foo.data);
+                foo = foo.next;
+            }
+
+            // We add final node value
+            valuesVector.Add(foo.data);
+
+            valuesVector.Sort();
+            foo = head;
+            int vectorCount = valuesVector.Count;
+
+            while(counter < vectorCount)
+            {
+                // This part starts from the beginning of the vector and goes up
+                foo.data = valuesVector[i];
+                counter++;
+                if(foo.next != null) foo = foo.next;
+
+                // In case the linked list is odd, we break the loop here
+                if(counter == vectorCount) break;
+                
+                // This part starts from the end of the vector and goes down
+                foo.data = valuesVector[vectorCount - i - 1];
+                counter++;
+                if(foo.next != null) foo = foo.next;
+
+                i++;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SingleLinkedList list = new SingleLinkedList();
+            list.AddNode(3);
+            list.AddNode(1);
+            list.AddNode(10);
+            list.AddNode(20);
+            list.AddNode(21);
+            list.AddNode(18);
+            list.AddNode(17);
+
+            list.Rearrange();
+        }
+    }
+}
---
+++ daily-800/README.md
@@ -0,0 +1,32 @@
+For some reason, I didn't receive a new problem today. So here's yesterday's problem.
+
+**Problem #800 [rated Medium] :**
+
+**This problem was asked by Fitbit.
+
+Given a linked list, rearrange the node values such that they appear in alternating low -> high -> low -> high ... form. For example, given 1 -> 2 -> 3 -> 4 -> 5, you should return 1 -> 3 -> 2 -> 5 -> 4.**
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+First of all, we implement a very simple LinkedList class and Node class to work with.
+
+Once we have a simple linked list filled with random values in each node, we begin to think about a solution to our problem.
+
+We assume that there is no duplicates for the values in nodes. Pretty much no constraints are given as to how we can engineer our solution.
+There are a number of ways we can solve the problem, from very simple solutions to more complex ones. We will use the fact that we can use auxilary space to simplify the problem !
+
+We create a vector that reflects all the values of the linked list. We then sort that vector using common sorting algorithms. Since we are able to directly change the values of the nodes, we are simply gonna assign them one by one going through the list, alternating between the start of our vector and the end of it.
+
+Here is an example : 
+We have a linked list 3 -> 8 -> 4 -> 10 -> 25 -> 31. We need to rearrange it since 10 -> 25 breaks our rule.
+We create a vector that reflects the values in the list [3,8,4,10,25,31]. We sort it [3,4,8,10,25,31].
+And now we alternate between the start and the end of our vector until there are no values left to pick : 3 -> 31 -> 4 -> 25 -> 8 -> 10.
+Our linked list is now alternating between low and high, starting with low.
+
+After some research, a more efficient solution can be found here : https://www.geeksforgeeks.org/rearrange-a-given-linked-list-in-place/
+
+The gist of it is : 
+1) Find the middle point using tortoise and hare method.
+2) Split the linked list into two halves using found middle point in step 1.
+3) Reverse the second half.
+4) Do alternate merge of first and second halves.
